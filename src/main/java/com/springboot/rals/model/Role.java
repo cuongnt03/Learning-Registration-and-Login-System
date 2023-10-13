f7435e5d9945a16f1b1c82b118359834ef9a6186
Nguyen Tuan Cuong (K17 HL) <cuongnthe171665@fpt.edu.vn>
2023-10-13 13:20:28 +0700

    Create JPA - Users and Roles

---
+++ src/main/java/com/springboot/rals/model/Role.java
@@ -0,0 +1,25 @@
+package com.springboot.rals.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    public Role() {
+    }
+
+    public Role(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/springboot/rals/model/User.java
@@ -0,0 +1,44 @@
+package com.springboot.rals.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Collection;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "user", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_Name")
+    private String firstName;
+    @Column(name = "last_Name")
+    private String lastName;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "password")
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "users_roles", // ten bang la users_role
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    private Collection<Role> roles;
+
+    public User(Long id, String firstName, String lastName, String email, String password, Collection<Role> roles) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.roles = roles;
+    }
+
+    public User() {
+    }
+}
